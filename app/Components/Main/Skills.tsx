237fa6345dd2c4b8917411a93c6397bf2f48d518
BAssal <baslm8496@gmail.com>
2024-02-24 01:39:16 +0200

    updating skills section

--- app/Components/Main/Skills.tsx
+++ app/Components/Main/Skills.tsx
@@ -1,9 +1,8 @@
 import {
-  Backend_skill,
+  Frameworks_skill,
   Frontend_skill,
-  Full_stack,
   Other_skill,
-  Skill_data,
+
 } from "@/app/constants";
 import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
@@ -12,24 +11,11 @@ import SkillText from "../sub/SkillsText";
 const Skills = () => {
   return (
     <section
-      id="skills"
+      id="Skills"
       className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
       style={{ transform: "scale(0.9" }}
     >
       <SkillText />
-
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Skill_data.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Frontend_skill.map((image, index) => (
           <SkillDataProvider
@@ -42,7 +28,7 @@ const Skills = () => {
         ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Backend_skill.map((image, index) => (
+        {Frameworks_skill.map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -52,17 +38,11 @@ const Skills = () => {
           />
         ))}
       </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Full_stack.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
+
+
+  
+     
+    
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Other_skill.map((image, index) => (
           <SkillDataProvider
@@ -75,7 +55,7 @@ const Skills = () => {
         ))}
       </div>
 
-      <div className="w-full h-full absolute">
+      <div className="w-full h-auto absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
           <video
             className="w-full h-auto"
--- app/Components/sub/SkillsText.tsx
+++ app/Components/sub/SkillsText.tsx
@@ -1,31 +1,34 @@
 "use client";
 import { motion } from "framer-motion";
 import {
-    slideInFromLeft,
-    slideInFromRight,
-    slideInFromTop,
-  } from "@/app/utils/motion";import { SparklesIcon } from "@heroicons/react/16/solid";
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/app/utils/motion";
+import { ClipboardIcon, SparklesIcon } from "@heroicons/react/16/solid";
 
 const SkillsText = () => {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <motion.div
         variants={slideInFromTop}
-        className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
       >
-        <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-        <h1 className="Welcome-text text-[13px]">
-          think better with next js 13
+        {/* <ClipboardIcon className="text-[#b49bff] mr-[10px] h-5 w-6" /> */}
+        <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-4">
+           My Skills
         </h1>
       </motion.div>
-      <motion.div variants={slideInFromLeft(0.5)} className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]">
-
-        making apps with modern technologies
+      <motion.div
+        variants={slideInFromLeft(0.5)}
+        className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
+      >
+        Making Apps With Modern Technologies
       </motion.div>
-      <motion.div variants={slideInFromRight(0.5)}        className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
->
-
-Never miss a task, deadline or idea
+      <motion.div
+        variants={slideInFromRight(0.5)}
+        className="cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
+      >
+        Never miss a task, deadline or idea
       </motion.div>
     </div>
   );
