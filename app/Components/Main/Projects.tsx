de190b56489527e3f851cca4be99dfe7fd02577c
BAssal <baslm8496@gmail.com>
2024-02-26 20:26:14 +0200

    uploading projects

--- app/Components/Main/Projects.tsx
+++ app/Components/Main/Projects.tsx
@@ -10,22 +10,56 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 mb-12">
+        <a  className="relative z-[150] " target="_blank" href="https://as3af-web.netlify.app/">
+        <ProjectCard
+          src="/assets/projects/eao.png"
+          title="A web for the Egyptian Ambulance Authority"
+          description=""
+        />
+        </a>
+    
+        <a  className="relative z-[150] " target="_blank" href="https://vistro.netlify.app/">
+        <ProjectCard
+          src="/assets/projects/vistro-web.png"
+          title="A vistro-web "
+          description=""
+        />
+        </a>
+        <a  className="relative z-[150] " target="_blank" href="https://game-killer.netlify.app/">
+        <ProjectCard
+          src="/assets/projects/game-killer.png"
+          title="Game-Killer"
+          description=""
+        />
+        </a>
+     
+      </div>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <a  className="relative z-[150] " target="_blank" href="https://hilink-web.netlify.app/">
         <ProjectCard
-          src="/assets/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/assets/projects/hilink.png"
+          title="A camping website"
+          description=""
+
         />
+        </a>
+    
+        <a  className="relative z-[150] " target="_blank" href="https://master--car-sitee.netlify.app/">
         <ProjectCard
-          src="/assets/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/assets/projects/car-hub.png"
+          title="a car website" 
+          description=""
         />
+        </a>
+        <a  className="relative z-[150] " target="_blank" href="https://systemira.com/">
         <ProjectCard
-          src="/assets/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/assets/projects/sysmira.png"
+          title=" a company wesbite"
+          description=""
         />
+        </a>
+     
       </div>
     </div>
   );
